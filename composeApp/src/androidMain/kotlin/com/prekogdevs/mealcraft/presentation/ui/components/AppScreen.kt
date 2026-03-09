5fb7141601004c3d3484599b658b506a04e7717e
gergely-szebenyi <gergelyszebenyi@gmail.com>
2026-03-09 11:18:27 +0100

    Fix padding

--- composeApp/src/androidMain/kotlin/com/prekogdevs/mealcraft/presentation/ui/components/AppScreen.kt
+++ composeApp/src/androidMain/kotlin/com/prekogdevs/mealcraft/presentation/ui/components/AppScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.prekogdevs.mealcraft.presentation.theme.MealCraftTheme
@@ -14,13 +15,16 @@ fun AppScreen(
 ) {
     MealCraftTheme(
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MealCraftTheme.colors.background)
-                    .padding(MealCraftTheme.spacing.space16)
-            ) {
-                screenContent()
+            Scaffold { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MealCraftTheme.colors.background)
+                        .padding(paddingValues = paddingValues)
+                        .padding(horizontal = MealCraftTheme.spacing.space16)
+                ) {
+                    screenContent()
+                }
             }
         }
     )
